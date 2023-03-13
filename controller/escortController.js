b03d42842137a8fa369ecc22d76c956a03cf745d
robinhood6996 <robinypb@yahoo.com>
2023-03-14 00:03:18 +0600

    escort profile changed

--- controller/escortController.js
+++ controller/escortController.js
@@ -1,9 +1,29 @@
-const { EscortBiography, PersonalDetails } = require("../models/escort.model");
+const EscortProfile = require("../models/escort.model");
+const { EscortProfileSchema } = require("../schema/escortSchema");
 
 exports.updateEscortBiography = async (req, res) => {
   try {
     if (req.body) {
-      res.status(200).json({ data: req.body });
+      let body = req.body;
+      let error;
+      Object.entries(body).forEach(([key, type]) => {
+        // console.log("key", key, EscortProfileSchema.tree.hasOwnProperty(key));
+        if (EscortProfileSchema.tree.hasOwnProperty(key)) {
+          error = null;
+        } else {
+          error = key;
+        }
+      });
+      if (error) {
+        res.status(400).json(`Unknown field "${error}"`);
+      } else {
+        let escort = new EscortProfile({
+          ...body,
+        });
+        await escort.save(body).then(() => {
+          res.status(200).json({ data: req.body });
+        });
+      }
     }
   } catch (err) {
     res.status(200).json({ error: "Internal server error" });
