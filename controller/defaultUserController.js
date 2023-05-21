e44b07851ec0184735a452add36d1b08948b5faf
robinhood6996 <robinypb@yahoo.com>
2023-05-22 00:14:10 +0600

    update user default

--- controller/defaultUserController.js
+++ controller/defaultUserController.js
@@ -4,7 +4,7 @@ exports.editDefaultUser = async (req, res) => {
   try {
     const { username } = req.user; // Assuming userId is passed as a parameter
     console.log("user", req.user);
-    const { name, phone, gender, ethnicity, nationality, country, city } =
+    const { name, phone, gender, ethnicity, nationality, country, city, age } =
       req.body;
 
     // Construct the update object with the provided fields
@@ -16,28 +16,28 @@ exports.editDefaultUser = async (req, res) => {
       nationality,
       country,
       city,
+      age,
     };
 
     // Find the user by ID and update the fields
-    const updatedUser = await defaultUser
-      .findOneAndUpdate(
-        { username: username },
-        { $set: { ...update } },
-        {
-          new: true,
-        }
-      )
-      .then((value) => {})
-      .catch((err) => {
-        console.log("error", err);
-      });
-
-    if (updatedUser) {
-      res
+    const existUser = await defaultUser.findOne({ username });
+    if (existUser) {
+      if (name) existUser.name = name;
+      if (phone) existUser.phone = phone;
+      if (gender) existUser.gender = gender;
+      if (ethnicity) existUser.ethnicity = ethnicity;
+      if (nationality) existUser.nationality = nationality;
+      if (country) existUser.country = country;
+      if (city) existUser.city = city;
+      if (age) existUser.age = age;
+      await existUser.save();
+      return res
         .status(200)
-        .json({ user: updatedUser, message: "User updated successfully" });
+        .json({ user: existUser, message: "User updated successfully" });
     } else {
-      res.status(404).json({ message: "User not found" });
+      return res
+        .status(404)
+        .json({ message: "User not found", statusCode: 404 });
     }
   } catch (error) {
     console.error("Error updating user:", error);
