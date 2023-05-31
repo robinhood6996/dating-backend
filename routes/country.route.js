140537ed1635d4e8032b6513e50669142634b391
robinhood6996 <robinypb@yahoo.com>
2023-06-01 02:03:03 +0600

    Country edit api added

--- routes/country.route.js
+++ routes/country.route.js
@@ -2,8 +2,16 @@ const express = require("express");
 const router = express.Router();
 const countryController = require("../controller/CountriesController");
 const upload = require("multer")();
-router.get("/", countryController.getAllCountries);
-router.post("/", upload.any(), countryController.createCountry);
-router.delete("/:id", countryController.deleteCountry);
+const { authenticate, adminAuth } = require("../middleware/tokenMiddleware");
+router.get("/", authenticate, countryController.getAllCountries);
+router.put(
+  "/:id",
+  authenticate,
+  adminAuth,
+  upload.any(),
+  countryController.editCountry
+);
+router.post("/", authenticate, upload.any(), countryController.createCountry);
+router.delete("/:id", authenticate, countryController.deleteCountry);
 
 module.exports = router;
