6a582f38b244c880df405b3df4a7db6d0134da5d
robinhood6996 <robinypb@yahoo.com>
2023-08-06 23:29:48 +0600

    update report module

--- controller/fakeController.js
+++ controller/fakeController.js
@@ -28,3 +28,40 @@ exports.addFakePhoto = async (req, res) => {
     res.status(500).json({ error: "Failed to add fakePhoto entry" });
   }
 };
+
+// Controller function to get all fakePhoto with limit, offset, and search
+exports.getAllFakePhotos = async (req, res) => {
+  const { limit, offset, search } = req.query;
+  const query = {};
+
+  // Apply search functionality if the "search" query parameter is provided
+  if (search) {
+    const searchRegex = new RegExp(search, "i"); // Case-insensitive search regex
+    query.$or = [
+      { name: searchRegex },
+      { reporterName: searchRegex },
+      { email: searchRegex },
+      { reporterEmail: searchRegex },
+      { username: searchRegex },
+      { reporterUsername: searchRegex },
+    ];
+  }
+
+  try {
+    const totalCount = await FakePhoto.countDocuments(query);
+
+    const fakePhotos = await FakePhoto.find(query)
+      .limit(parseInt(limit) || 20) // Default limit is 10 if not provided or not a valid number
+      .skip(parseInt(offset) || 0) // Default offset is 0 if not provided or not a valid number
+      .exec();
+
+    res.status(200).json({
+      fakePhotos,
+      totalCount,
+      count: fakePhotos.length,
+    });
+  } catch (error) {
+    console.error("Error fetching fakePhotos:", error);
+    res.status(500).json({ error: "Failed to fetch fakePhotos" });
+  }
+};
--- routes/fake.route.js
+++ routes/fake.route.js
@@ -5,5 +5,6 @@ const { authenticate, adminAuth } = require("../middleware/tokenMiddleware");
 const upload = require("multer")();
 
 router.post("/", upload.any(), authenticate, queryController.addFakePhoto);
+router.get("/", upload.any(), authenticate, queryController.getAllFakePhotos);
 
 module.exports = router;
